5c0503eff0c0bcde058a7f176b3b4ff1e5b71467
hzqd <hzqelf@yeah.net>
2023-06-15 18:44:54 +0800

    Extract numbers from String

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::process;
 use aoko::{standard::functions::{ext::StdAnyExt, fun::read_line}, no_std::pipelines::tap::Tap};
+use regex::Regex;
+
 fn main() {
+    let r = Regex::new(r"-?[0-9]*\.?[0-9]+").unwrap();
+
     loop {
         read_line()
             .tap(|s| if s.trim_end().is_empty() { process::exit(0) })
             .split_whitespace()
-            .map(|s| s.chars().filter(|c| ('0'..='9').contains(c) || c == &'.').collect::<String>())
-            .filter_map(|s| s.parse::<f64>().ok())
+            .flat_map(|s| r.find_iter(s))
+            .filter_map(|m| m.as_str().parse::<f64>().ok())
             .sum::<f64>()
             .echo();
     }
