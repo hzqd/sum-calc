930f81ff65f9fa0ee253eda102705fccdde381be
hzqd <hzqelf@yeah.net>
2023-06-16 03:05:57 +0800

    Optimize software size

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 use std::process;
 use aoko::{standard::functions::{ext::StdAnyExt, fun::read_line}, no_std::pipelines::tap::Tap};
-use regex::Regex;
-
 fn main() {
-    let r = Regex::new(r"-?[0-9]*\.?[0-9]+").unwrap();
-
     loop {
         read_line()
             .tap(|s| if s.trim_end().is_empty() { process::exit(0) })
-            .split_whitespace()
-            .flat_map(|s| r.find_iter(s))
-            .filter_map(|m| m.as_str().parse::<f64>().ok())
+            .split(|c: char| !c.is_digit(10) && c != '.' && c != '-')
+            .filter_map(|s| s.parse::<f64>().ok())
             .sum::<f64>()
             .echo();
     }
