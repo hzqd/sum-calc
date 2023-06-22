5ede6f03131f3f1b9db9da2a827d19c37c7426e3
hzqd <hzqelf@yeah.net>
2023-06-22 10:12:37 +0800

    Special case handling: extract string ‘a1-2b’ as numbers 1 and 2

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ fn main() {
     loop {
         read_line()
             .tap(|s| if s.trim_end().is_empty() { process::exit(0) })
-            .split(|c: char| !c.is_digit(10) && c != '.' && c != '-')
+            .split(|c: char| !c.is_ascii_digit() && c != '.' && c != '-')
+            .flat_map(|s| if !s.starts_with('-') && s.contains('-') { s.split('-').collect() } else { vec![s] })
             .filter_map(|s| s.parse::<f64>().ok())
             .sum::<f64>()
             .echo();
