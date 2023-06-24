1ae4bc6f3848858c374351dbba0ff346a299ce94
hzqd <hzqelf@yeah.net>
2023-06-24 14:13:17 +0800

    Performance optimization: avoid allocating values on the heap

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::process;
-use aoko::{standard::functions::{ext::StdAnyExt, fun::read_line}, no_std::pipelines::tap::Tap};
+use aoko::{standard::functions::{ext::StdAnyExt, fun::read_line}, no_std::{pipelines::tap::Tap, cps::operators::Op}};
 fn main() {
     loop {
         read_line()
             .tap(|s| if s.trim_end().is_empty() { process::exit(0) })
             .split(|c: char| !c.is_ascii_digit() && c != '.' && c != '-')
-            .flat_map(|s| if !s.starts_with('-') && s.contains('-') { s.split('-').collect() } else { vec![s] })
+            .flat_map(|s| s.starts_with('-').not_c(|b| s.split(move |c| b && c == '-')))
             .filter_map(|s| s.parse::<f64>().ok())
             .sum::<f64>()
             .echo();
