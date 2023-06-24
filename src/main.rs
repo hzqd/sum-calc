b22f66f53f3ad6aba296548511579c734f05bf5a
hzqd <hzqelf@yeah.net>
2023-06-24 17:16:49 +0800

    Add hints for invalid nums, such as '1..2', to prevent input errors

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use std::process;
-use aoko::{standard::functions::{ext::StdAnyExt, fun::read_line}, no_std::{pipelines::tap::Tap, cps::operators::Op}};
+use std::{process, ops::Not};
+use aoko::{standard::functions::fun::read_line, no_std::{pipelines::{tap::Tap, pipe::Pipe}, cps::operators::Op}};
 fn main() {
     loop {
         read_line()
             .tap(|s| if s.trim_end().is_empty() { process::exit(0) })
-            .split(|c: char| !c.is_ascii_digit() && c != '.' && c != '-')
+            .split(|c: char| c.is_ascii_digit().not() && c != '.' && c != '-')
             .flat_map(|s| s.starts_with('-').not_c(|b| s.split(move |c| b && c == '-')))
-            .filter_map(|s| s.parse::<f64>().ok())
-            .sum::<f64>()
-            .echo();
+            .filter(|s| s.is_empty().not())
+            .try_fold(0.0, |acc, s| s.parse().map_or(Err(s), |n: f64| Ok(acc + n)))
+            .pipe(|r| match r {
+                Ok(v) => println!("{v}"),
+                Err(e) => println!("Extract numeric error from '{e}'")
+            });
     }
 }
