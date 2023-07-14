33be9ba59e4a621f5e7b84391b70c9c0ac0d1818
hzqd <hzqelf@yeah.net>
2023-07-15 02:54:50 +0800

    Process data in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::{process, ops::Not};
 use aoko::{standard::functions::fun::read_line, no_std::{pipelines::{tap::Tap, pipe::Pipe}, cps::operators::Op}};
+use rayon::{str::ParallelString, prelude::ParallelIterator};
 fn main() {
     loop {
         read_line()
             .tap(|s| if s.trim_end().is_empty() { process::exit(0) })
-            .split(|c: char| c.is_ascii_digit().not() && c != '.' && c != '-')
-            .flat_map(|s| s.starts_with('-').not_c(|b| s.split(move |c| b && c == '-')))
+            .par_split(|c: char| c.is_ascii_digit().not() && c != '.' && c != '-')
+            .flat_map_iter(|s| s.starts_with('-').not_c(|b| s.split(move |c| b && c == '-')))
             .filter(|s| s.is_empty().not())
-            .try_fold(0.0, |acc, s| s.parse().map_or(Err(s), |n: f64| Ok(acc + n)))
+            .map(|s| s.parse().map_err(|_| s))
+            .try_reduce(|| 0.0, |a, n| Ok(a + n))
             .pipe(|r| match r {
                 Ok(v) => println!("{v}"),
                 Err(e) => println!("Extract numeric error from '{e}'")
