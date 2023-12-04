d0b8d81765426e974307d0dcfbb1137caef1fcdf
hzqd <hzqelf@yeah.net>
2023-12-04 22:01:36 +0800

    Disambiguate minus signs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::{process, ops::Not};
-use aoko::{standard::functions::fun::read_line, no_std::{pipelines::{tap::Tap, pipe::Pipe}, cps::operators::Op}};
+use aoko::{standard::functions::fun::read_line, no_std::pipelines::{tap::Tap, pipe::Pipe}};
 use rayon::{str::ParallelString, prelude::ParallelIterator};
 fn main() {
     loop {
         read_line()
             .tap(|s| if s.trim_end().is_empty() { process::exit(0) })
             .par_split(|c: char| c.is_ascii_digit().not() && c != '.' && c != '-')
-            .flat_map_iter(|s| s.starts_with('-').not_c(|b| s.split(move |c| b && c == '-')))
             .filter(|s| s.is_empty().not())
             .map(|s| s.parse().map_err(|_| s))
             .try_reduce(|| 0.0, |a, n| Ok(a + n))
