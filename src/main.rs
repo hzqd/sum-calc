1f5388686e278f90fb67e06947bd548e9524c058
hzqd <hzqelf@yeah.net>
2023-12-05 17:00:26 +0800

    Upgrade

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::{process, ops::Not};
+use std::process;
 use aoko::{standard::functions::fun::read_line, no_std::pipelines::{tap::Tap, pipe::Pipe}};
 use rayon::{str::ParallelString, prelude::ParallelIterator};
+
 fn main() {
     loop {
         read_line()
             .tap(|s| if s.trim_end().is_empty() { process::exit(0) })
-            .par_split(|c: char| c.is_ascii_digit().not() && c != '.' && c != '-')
-            .filter(|s| s.is_empty().not())
+            .par_split(|c: char| !c.is_ascii_digit() && c != '.' && c != '-')
+            .filter(|s| !s.is_empty())
             .map(|s| s.parse().map_err(|_| s))
             .try_reduce(|| 0.0, |a, n| Ok(a + n))
             .pipe(|r| match r {
