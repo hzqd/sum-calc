c9a77ebad589e362a6b09c6b1e41378901a69145
hzqd <hzqelf@yeah.net>
2023-12-23 08:35:39 +0800

    Change to closure

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::stdin;
+use std::{io::stdin, cell::RefCell};
 
 fn cangjie_official_language_design_member_liu_jun_jie_implementation_based_on_inite_state_machine(text: String) -> f64 {
     let mut states = [[-1; 128]; 3];
@@ -6,23 +6,19 @@ fn cangjie_official_language_design_member_liu_jun_jie_implementation_based_on_i
     states[1][45..58].copy_from_slice(&[-1, 2, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
     states[2][45..58].copy_from_slice(&[-1, -1, -1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]);
 
-    let mut i = 0;
-    let mut sum = 0.0;
-    while i < text.len() {
+    let i = RefCell::new(0);
+    (0..).take_while(|_| *i.borrow() < text.len()).fold(0.0, |acc, _| {
         let mut s = 0i8;
-        while i < text.len() && states[s as usize][text.as_bytes()[i] as usize] == -1 {
-            i += 1
+        while *i.borrow() < text.len() && states[s as usize][text.as_bytes()[*i.borrow()] as usize] == -1 {
+            *i.borrow_mut() += 1
         }
-        let begin = i;
-        while i < text.len() && states[s as usize][text.as_bytes()[i] as usize] != -1 {
-            s = states[s as usize][text.as_bytes()[i] as usize];
-            i += 1;
+        let begin = *i.borrow();
+        while *i.borrow() < text.len() && states[s as usize][text.as_bytes()[*i.borrow()] as usize] != -1 {
+            s = states[s as usize][text.as_bytes()[*i.borrow()] as usize];
+            *i.borrow_mut() += 1;
         }
-        if let Some(num) = text[begin..i].parse::<f64>().ok() {
-            sum += num
-        }
-    }
-    sum
+        acc + text[begin..*i.borrow()].parse::<f64>().unwrap_or_default()
+    })
 }
 
 fn main() {
